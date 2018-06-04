ed50990da36e41ad2892fe15160d5272d48d7c71
dreamshader <dreamshader@gmx.de>
2018-06-04 18:39:55 +0200

    change license to apache 2.0 licence because of new owner of git is microsoft

--- smartTFT/smartTFT.ino
+++ smartTFT/smartTFT.ino
@@ -1,6 +1,20 @@
 /***************************************************
  smartTFT ...
- (C) 2018
+
+    Copyright (C) 2018 Dreamshader (aka Dirk Schanz)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
 Der Sketch verwendet 23478 Bytes (76%) des Programmspeicherplatzes. Das Maximum sind 30720 Bytes.
 Globale Variablen verwenden 1320 Bytes (64%) des dynamischen Speichers, 728 Bytes für lokale Variablen verbleiben. Das Maximum sind 2048 Bytes.
 
